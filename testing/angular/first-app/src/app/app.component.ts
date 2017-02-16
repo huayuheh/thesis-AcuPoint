13d311d840f48537c473b9e95616f01f19cc10b7
hanaheh <hanaheh128@gmail.com>
2017-02-15 21:28:06 -0800

    add a new component

--- testing/angular/first-app/src/app/app.component.ts
+++ testing/angular/first-app/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>AcuPoint</h1>
+    <fa-other></fa-other>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'I changed it!';
 }
