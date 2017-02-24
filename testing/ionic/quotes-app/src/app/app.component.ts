34dbe47ec67867e4b49fd17c5643e847b57e4b8f
hanaheh <hanaheh128@gmail.com>
2017-02-23 20:24:03 -0800

    create tabs

--- testing/ionic/quotes-app/src/app/app.component.ts
+++ testing/ionic/quotes-app/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import {FavoritesPage} from "../pages/favorites/favorites";
 
+import { TabsPage } from "../pages/tabs/tabs";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = TabsPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- testing/ionic/quotes-app/src/app/app.module.ts
+++ testing/ionic/quotes-app/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-
+import { FavoritesPage } from '../pages/favorites/favorites';
+import {LibraryPage} from "../pages/library/library";
+import {QuotesPage} from "../pages/quotes/quotes";
+import {QuotePage} from "../pages/quote/quote";
+import { SettingPage } from "../pages/setting/setting";
+import { TabsPage } from "../pages/tabs/tabs";
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingPage,
+    TabsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +23,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingPage,
+    TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
