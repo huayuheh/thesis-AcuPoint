c0b55124bc92bcb0167b5cb381184f3071e8b306
hanaheh <hanaheh128@gmail.com>
2017-02-26 21:48:32 -0800

    Create quote page and grab data from database

--- testing/ionic/quotes-app/src/app/app.component.ts
+++ testing/ionic/quotes-app/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { HomePage } from '../pages/home/home';
-import {FavoritesPage} from "../pages/favorites/favorites";
 
 import { TabsPage } from "../pages/tabs/tabs";
 
--- testing/ionic/quotes-app/src/pages/quotes/quotes.ts
+++ testing/ionic/quotes-app/src/pages/quotes/quotes.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import {  NavParams, AlertController } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
 
-/*
-  Generated class for the Quotes page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html'
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit{
+  quoteGroup: {category: string, quotes: Quote[], icon: string };
+  constructor(
+    private navParams:NavParams,
+    private  alertCtrl:AlertController){}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  ngOnInit(){
+    this.quoteGroup = this.navParams.data;
   }
 
+  // ionViewDidLoad(){
+  //   this.quoteGroup = this.navParams.data;
+  // add elvis operator (?) in template to use this approach
+  // }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title:'Add quote',
+      subTitle:'Are you sure?',
+      message:'Are you sure you want to add the quote?'
+    });
+
+    alert.present();
+  }
 }
