f81264e7a5f0a0888526796ba55d8035a1a02e90
hanaheh <hanaheh128@gmail.com>
2017-03-23 22:39:04 -0700

    RGB led

--- testing/sensor/button-led-bak/turn-on.js
+++ testing/sensor/button-led-bak/turn-on.js
@@ -3,19 +3,26 @@ var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
   button = new Gpio(18, 'in', 'both'),	//setup GPIO17 as output
   led = new Gpio(4, 'out'),      //setup GPIO27 as output
   ledState = 0, 		  //internal variable to track LED state (1 = on, 0 = off)
-  Timer = 0;
+  timer = 0;
 
 button.setActiveLow( true );		//optional to reverse button value
 
 button.watch(function(err, value) {	//watch button changes
 	if (value == true){
 		timer = timer +1 ;
-
+        console.log('timer:'+ timer);
 	}else{
-		led.writeSync( 0 );
-		console.log('Button is OFF');
+	//	led.writeSync( 0 );
+	//	console.log('Button is OFF');
 	}
-        led.writeSync( timer%2 );
+    if ( timer % 2 == 0 ){
+        led.writeSync( 0 );
+	}else{
+        led.writeSync( 1 );
+	}
+
+
+
 
 });
 
