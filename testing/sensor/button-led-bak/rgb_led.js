8c784f7915ee88cd413d0e8c7f8369f8b91dd42b
hanaheh <hanaheh128@gmail.com>
2017-03-23 22:12:16 -0700

    button with led and RGB led

---
+++ testing/sensor/button-led-bak/rgb_led.js
@@ -0,0 +1,22 @@
+var RgbChannel = require('rpi-rgb').Channel;
+var Colour = require('rpi-rgb').Colour;
+
+var channel1 = new RgbChannel(25,24,23);
+
+var red = new Colour(100,0,0);
+var softRed = new Colour(10,0,0);
+var blue = new Colour(0,100,0);
+var white = new Colour(100,100,100);
+var yellow = new Colour(100,100,0);
+
+// Start by fading to blue.
+channel1.fadeRgb(blue, 2000, function() {
+    // When that's done, strobe.
+    channel1.strobeRgb(white, 18, 1000, function() {
+        // After strobing, fade to yellow.
+        channel1.fadeRgb(yellow, 700);
+    });
+});
+
+// After the timeout, all the above is likely done, so start pulsing red.
+setTimeout(function(thisobj) { thisobj.pulseRgb(softRed, red, 800, 1500); }, 7000, channel1);
--- testing/sensor/button-led-bak/turn-on.js
+++ testing/sensor/button-led-bak/turn-on.js
@@ -1,21 +1,22 @@
 
 var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
-  button = new Gpio(17, 'in', 'both'),	//setup GPIO17 as output
-  led = new Gpio(27, 'out'),      //setup GPIO27 as output
-  ledState = 0; 		  //internal variable to track LED state (1 = on, 0 = off)
-
-
+  button = new Gpio(18, 'in', 'both'),	//setup GPIO17 as output
+  led = new Gpio(4, 'out'),      //setup GPIO27 as output
+  ledState = 0, 		  //internal variable to track LED state (1 = on, 0 = off)
+  Timer = 0;
 
 button.setActiveLow( true );		//optional to reverse button value
 
 button.watch(function(err, value) {	//watch button changes
 	if (value == true){
-		led.writeSync( 1 );
-		console.log('Button is ON' );
+		timer = timer +1 ;
+
 	}else{
 		led.writeSync( 0 );
 		console.log('Button is OFF');
 	}
+        led.writeSync( timer%2 );
+
 });
 
 process.on('SIGINT', function(){
